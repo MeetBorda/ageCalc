e08e464cdcecf8e9ce8537d8b3d2f996e31b28b1
Meet Borda <borda.meet1@gmail.com>
2019-04-09 21:07:13 +0530

    FIrst Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+ class App extends Component {
+currentTime(){
+  return new Date();
+};
+birth(){
+  return new Date("1998-11-13 07:15:00")
   }
+constructor(props) {
+    super(props);
+      this.state = {
+      birthDay : this.birth(),
+      time : this.currentTime(),
+      age : undefined,
+    };
+  }
+
+  
+render() {
+let years = this.state.age / 31556900000
+let majorminor=years.toFixed(9).toString().split('.');
+
+
+console.log(years)
+  return ( 
+  <div className = "App" >
+    <div className = "try">
+      <h1>{majorminor[0]}<span>.{majorminor[1]}</span></h1>
+    </div>
+  </div>
+  );
+}
+componentDidMount(){
+  this.interval = setInterval(()=>
+  {
+    this.setState({age:this.currentTime()-this.birth()})
+    console.log(this.state.age)}
+    ,100)
+}
+componentWillUnmount(){
+  clearInterval(this.interval)
+}
 }
 
 export default App;
